4bcf08ff1c3e855e3fb794c32e71056d6b6876c8
vikas verma <vikas.verma@tothenew.com>
2016-02-10 16:37:16 +0530

    again push over

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,9 +1,10 @@
 dataSource {
     pooled = true
     jmxExport = true
-    driverClassName = "org.h2.Driver"
-    username = "sa"
-    password = ""
+    dialect = "org.hibernate.dialect.MySQL5InnoDBDialect"
+    dbCreate ="update"
+    driverClassName = "com.mysql.jdbc.Driver"
+
 }
 hibernate {
     cache.use_second_level_cache = true
@@ -18,8 +19,10 @@ hibernate {
 environments {
     development {
         dataSource {
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
+            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
             url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+            username = "sa"
+            password = ""
         }
     }
     test {
